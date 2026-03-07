9e353484f73591bebe2d20165d2bf76318969c1e
Bhavankumar2001 <143491829+Bhavankumar2001@users.noreply.github.com>
2026-03-07 22:31:27 +0530

    Add files via upload

---
+++ backend_portfolio/src/main/java/com/portfolio/controller/EmailService.java
@@ -0,0 +1,28 @@
+package com.portfolio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+
+@Service
+public class EmailService {
+
+	public void sendContactEmail(String name, String email, String message) throws ResendException {
+
+		String apiKey = System.getenv("RESEND_API_KEY");
+		Resend resend = new Resend(apiKey);
+
+		CreateEmailOptions params = CreateEmailOptions.builder().from("Portfolio <onboarding@resend.dev>")
+				.to("bhavankumar1862001@gmail.com").subject("New Portfolio Contact")
+				.html("<h3>New Message</h3>" + "<p><b>Name:</b> " + name + "</p>" + "<p><b>Email:</b> " + email + "</p>"
+						+ "<p><b>Message:</b> " + message + "</p>")
+				.build();
+
+		resend.emails().send(params);
+	}
+}
